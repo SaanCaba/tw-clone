ba655c44de18a00ff34c512693c45e7321bc5f43
SaanCaba <saantyc12.caballero@gmail.com>
2023-08-25 15:26:25 -0300

    add loader

--- src/app/components/Forms/CreatePost/index.tsx
+++ src/app/components/Forms/CreatePost/index.tsx
@@ -1,28 +1,41 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { IconWorld } from '@tabler/icons-react'
 import Avatar from '../../Helpers/Avatar'
-// import { Supabase } from '@/supabase/Supabase'
+import Loading from '../../Loading'
 
 interface Props {
   avatar: string
 }
 
 function CreatePost({ avatar }: Props) {
+  const [post, setPost] = useState('')
+  const [loading, setLoading] = useState(false)
   const inputRef = useRef<null | HTMLInputElement>(null)
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
     const data = {
-      content: inputRef.current?.value
+      content: post
     }
-    await fetch('/api/create-post', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
+    try {
+      setLoading(true)
+      await fetch('/api/create-post', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+    } catch (error) {
+    } finally {
+      setLoading(false)
+      setPost('')
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPost(e.target.value)
   }
 
   return (
@@ -36,6 +49,8 @@ function CreatePost({ avatar }: Props) {
           className='w-full bg-transparent text-xl focus:outline-none'
           placeholder='¡¿Qué está pasando?!'
           type='text'
+          onChange={handleChange}
+          value={post}
         />
 
         <span className='flex gap-2 text-green'>
@@ -43,10 +58,15 @@ function CreatePost({ avatar }: Props) {
         </span>
         <hr />
         <button
+          disabled={post.length === 0}
           type='submit'
-          className='rounded-full bg-green font-bold bg-red-300 px-5 py-2 self-end'
+          className={`${
+            post.length === 0
+              ? 'bg-green/25 cursor-not-allowed'
+              : 'bg-green hover:bg-green/70'
+          } rounded-full font-bold px-5 py-2 self-end transition-all duration-200 ease-in flex`}
         >
-          Postear
+          {loading && <Loading />} Postear
         </button>
       </form>
     </div>
